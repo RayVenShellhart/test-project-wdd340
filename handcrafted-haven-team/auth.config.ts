a531c63b7a54db537c23b94bf8c4bfb373d6c27a
RayVenShellhart <rayvenshellhart@gmail.com>
2025-12-08 15:05:47 -0800

    auth-redirect

--- handcrafted-haven-team/auth.config.ts
+++ handcrafted-haven-team/auth.config.ts
@@ -15,7 +15,7 @@ export const authConfig = {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(`${appUrl}/dashboard`);
+        return Response.redirect(new URL(`${appUrl}/dashboard`));
       }
       return true;
     },
